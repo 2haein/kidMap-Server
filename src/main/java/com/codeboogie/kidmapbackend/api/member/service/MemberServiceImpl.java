5938b67b74adf2c9d06563f3f6042af0524163d3
2haein <eehaein2@gmail.com>
2022-03-31 17:05:23 +0900

    MemberService 구현 파일 추가

---
+++ src/main/java/com/codeboogie/kidmapbackend/api/member/service/MemberServiceImpl.java
@@ -0,0 +1,35 @@
+package com.codeboogie.kidmapbackend.api.member.service;
+
+import com.codeboogie.kidmapbackend.common.member.domain.dto.MemberDTO;
+import com.codeboogie.kidmapbackend.common.member.domain.model.Member;
+import com.codeboogie.kidmapbackend.common.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService{
+
+    @Autowired
+    private final MemberRepository memberRepository;
+
+    @Override
+    public boolean createMember(MemberDTO memberDTO){
+        try{
+            Member kidmapMember = new Member();
+            
+            Member member = memberRepository.findByUserId(memberDTO.getUserId());
+            if (member == null) {
+                memberRepository.save(kidmapMember);
+            }
+
+        } catch (Exception e){
+            System.out.println("이전에 가입한 사용자 로그인 중......");
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+}
