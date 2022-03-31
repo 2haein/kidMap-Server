9f70ba3329db62d14270820d3144b5b627cd1956
2haein <eehaein2@gmail.com>
2022-03-31 17:12:51 +0900

    Member DTO 파일 정의

---
+++ src/main/java/com/codeboogie/kidmapbackend/common/member/domain/dto/MemberDTO.java
@@ -0,0 +1,12 @@
+package com.codeboogie.kidmapbackend.common.member.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDTO {
+    private String _id;
+
+    private String userId;
+
+    private String userName;
+}
