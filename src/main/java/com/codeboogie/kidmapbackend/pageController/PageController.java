fb3f39ec65a4bc86235893e7bbcb6b822bd55e80
2haein <eehaein2@gmail.com>
2022-04-27 13:48:29 +0900

    application.yml 설정 및 핸들러 조정

--- src/main/java/com/codeboogie/kidmapbackend/pageController/PageController.java
+++ src/main/java/com/codeboogie/kidmapbackend/pageController/PageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-    @GetMapping("/address")
+    @GetMapping("/kakaoAddr")
     public String addressPage(){
         return "kakaoAddr.html";
     }
