d46a3a08fe86d7ef49c48d649e5eb69c91f9f848
2haein <eehaein2@gmail.com>
2022-05-12 02:13:27 +0900

    심부름 스키마 재설정 필드 추가

--- src/main/java/com/codeboogie/kidmapbackend/api/member/controller/ErrandController.java
+++ src/main/java/com/codeboogie/kidmapbackend/api/member/controller/ErrandController.java
@@ -8,8 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.*;
 
 @RestController
 @RequestMapping("api")
@@ -20,7 +19,7 @@ public class ErrandController {
 
 
     @RequestMapping(path="/registerErrand", method={ RequestMethod.GET, RequestMethod.POST })
-    public @ResponseBody void updateMember(@RequestBody HashMap<String, String> data, ErrandDTO errandDTO) {
+    public @ResponseBody void registerErrand(@RequestBody HashMap<String, String> data, ErrandDTO errandDTO) {
         System.out.println("안드로이드 -> 서버 /registerErrand"+ data+ ":"+ data.get("E_content") + " , " +data.get("UUID"));
 
         try {
@@ -32,18 +31,25 @@ public class ErrandController {
             String UUID = String.valueOf(data.get("UUID"));
             Double target_latitude = Double.valueOf((data.get("target_latitude")));
             Double target_longitude = Double.valueOf((data.get("target_longitude")));
+            String target_name = String.valueOf(data.get("target_name"));
             Double start_latitude = Double.valueOf((data.get("start_latitude")));
             Double start_longitude = Double.valueOf((data.get("start_longitude")));
+            String start_name = String.valueOf(data.get("start_name"));
             boolean checking = Boolean.parseBoolean(data.get("checking"));
+            List<String> quest = new ArrayList<String>();
+            quest = Arrays.asList((data.get("quest")));
 
             errandDTO.setUUID(UUID);
             errandDTO.setE_date(E_date);
             errandDTO.setE_content(E_content);
             errandDTO.setTarget_latitude(target_latitude);
             errandDTO.setTarget_longitude(target_longitude);
+            errandDTO.setTarget_name(target_name);
             errandDTO.setStart_latitude(start_latitude);
             errandDTO.setStart_longitude(start_longitude);
+            errandDTO.setStart_name(start_name);
             errandDTO.setChecking(checking);
+            errandDTO.setQuest(quest);
 
             errandService.registerErrand(errandDTO);
 
--- src/main/java/com/codeboogie/kidmapbackend/common/member/domain/dto/ErrandDTO.java
+++ src/main/java/com/codeboogie/kidmapbackend/common/member/domain/dto/ErrandDTO.java
@@ -22,9 +22,15 @@ public class ErrandDTO {
 
     private double target_longitude;   // 목표 장소 경도
 
+    private String target_name; // 목표 장소 이름
+
     private double start_latitude;   // 출발 장소 위도
 
     private double start_longitude;   // 출발 장소 경도
 
+    private String start_name; // 출발지 장소 이름
+
     private boolean checking;  //   위험요소 파악했는지 여부
+
+    private List<String> quest; // 퀘스트 목록
 }
